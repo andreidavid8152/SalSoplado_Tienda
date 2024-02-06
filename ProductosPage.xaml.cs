fe40a370ea66ec65c9384f680e82c33f881c11b0
andreidavid8152 <andreidavid3399@gmail.com>
2024-02-06 16:49:14 -0500

    Vista productos conectada a API

--- ProductosPage.xaml.cs
+++ ProductosPage.xaml.cs
@@ -1,16 +1,50 @@
+using SalSoplado_Tienda.Models;
+using SalSoplado_Usuario;
+using SalSoplado_Usuario.Services;
+
 namespace SalSoplado_Tienda;
 
 public partial class ProductosPage : ContentPage
 {
+
+    private readonly APIService _api;
+    private string token = Preferences.Get("UserToken", string.Empty);
+    private int LocalId { get; set; }
+
     public ProductosPage()
     {
         InitializeComponent();
+        _api = App.ServiceProvider.GetService<APIService>();
     }
 
-    private async void OnFrameTapped(object sender, EventArgs e)
+    protected override void OnAppearing()
     {
-        await DisplayAlert("Éxito", "Inicio de sesión exitoso", "OK");
+        base.OnAppearing();
+        LocalId = SharedData.SelectedLocalId;
+        CargarProductos();
     }
 
+    private async void OnEditTapped(object sender, EventArgs e)
+    {
+        await DisplayAlert("Éxito", "Editar", "OK");
+    }
+
+    private async void OnDeleteTapped(object sender, EventArgs e)
+    {
+        await DisplayAlert("Éxito", "Eliminar", "OK");
+    }
+
+    private async void CargarProductos()
+    {
+        try
+        {
+            var productos = await _api.ObtenerProductosPorLocal(LocalId, token); // Asume que este método existe en APIService y devuelve una lista de ProductoLocalDetalle
+            productosCollectionView.ItemsSource = productos;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
 
 }
